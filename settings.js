2d395cd669abd0e5a7e21cb8e9cae6bc8567511f
Bugbots3 <bugbots3@gmail.com>
2024-08-03 17:41:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,26 +4,26 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "916909137213"
-global.ownername = "David Cyril"
+global.ownernomer = "2349132961987"
+global.ownername = "KOSI"
 global.ytname = "David Cyril"
 global.socialm = "GitHub: DeeCeeXxx"
 global.location = "Africa, Nigeria, Lagos"
 
-global.ownernumber = '2347043759577,2349066528353,254782572110'  //creator number
+global.ownernumber = '2349132961987'  //creator number
 global.ownername = 'David Cyril' //owner name
-global.botname = '🕷Venom-2🕷' //name of the bot
+global.botname = 'KOSI-V3-BUG' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
 global.author = 'David_Cyril\n\nContact: +2349066528353'
 
 //console view/theme
-global.themeemoji = '🪀'
+global.themeemoji = '💀'
 global.wm = "David Cyril Tech."
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L'
+global.link = 'https://whatsapp.com/channel/0029VaeMMwy3rZZkRlrJKl1q'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
